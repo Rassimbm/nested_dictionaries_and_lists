99ac91aee36efe8063a9f09e1b37f5fcf04ae970
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-24 17:40:16 -0500

    completed task 2

--- nested_dicts_lists.py
+++ nested_dicts_lists.py
@@ -1,4 +1,6 @@
-# 1 -Update Values in Dictionaries and Lists
+"""
+1 - Update Values in Dictionaries and Lists
+"""
 x = [ [5,2,3], [10,8,9] ] 
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
@@ -24,3 +26,30 @@ z = [ {'x': 10, 'y': 20} ]
 # D- Change the value 20 in z to 30
 z[0]["y"] = 30
 print(z)
+
+"""
+2 - Iterate Through a List of Dictionaries
+Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+"""
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(list):
+    for dic in range(0, len(list)):
+        output = ""
+        for key, val in list[dic].items():
+            output += f"{key} - {val}, "
+        print(output)
+iterateDictionary(students)
+"""
+should output: (it's okay if each key-value pair ends up on 2 separate lines;
+bonus to get them to appear exactly as below!)
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+"""
